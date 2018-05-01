5b9419b4bae307a0275cc36450c5f5f58235ae12
KevinTsaiii <kevin.t@gapp.nthu.edu.tw>
2018-05-02 02:11:26 +0800

    Add ping function

--- midterm/midterm_project_103061101/main.cpp
+++ midterm/midterm_project_103061101/main.cpp
@@ -11,6 +11,8 @@ DigitalIn speech_IO7(D5);
 DigitalIn speech_IO8(D6);
 DigitalIn speech_IO9(D8);
 
+DigitalInOut pin10(D10);
+
 PwmOut pin12(D12), pin11(D11), buzzer(D3);
 
 uLCD_4DGL uLCD(D1, D0, D2);
@@ -24,13 +26,12 @@ float FXOS8700CQ_read_X();
 float FXOS8700CQ_read_Y();
 float FXOS8700CQ_read_Z();
 
-//parallax_servo *servo0_ptr, *servo1_ptr;
-
 int main()
 {
     parallax_servo servo_left(pin11);   //Left wheel
     parallax_servo servo_right(pin12);   //Right wheel
     servo_ticker.attach(&servo_control, .5);
+    parallax_ping ping1(pin10);
 
     FXOS8700CQ_enable();
     int slope;
@@ -40,19 +41,30 @@ int main()
         //go 0001
         if (speech_IO7 == 1 && speech_IO8 == 0 && speech_IO9 == 0)
         {
-            //Red
-            redLED = 0;
-            greenLED = 1;
-            blueLED = 1;
+            if(ping1 > 15)
+            {
+                //Red
+                redLED = 0;
+                greenLED = 1;
+                blueLED = 1;
 
-            //Move forward
-            servo_left = 100;
-            servo_right = -100;
+                //Move forward
+                servo_left = 100;
+                servo_right = -100;
 
-            uLCD.cls();
-            uLCD.printf("\nMoving forward\n");
+                uLCD.cls();
+                uLCD.printf("\nMoving forward\n");
 
-            xbee.printf("Moving forward \n");
+                xbee.printf("Moving forward \n");
+            }
+            else
+            {
+                uLCD.cls();
+                uLCD.printf("\nObject detected\n");
+                uLCD.printf("\nMove back\n");
+
+                xbee.printf("Object detected\n");
+            }
 
             wait(1);
         }
@@ -73,26 +85,36 @@ int main()
             uLCD.printf("\nMoving backward\n");
 
             xbee.printf("Moving backward\n");
-
             wait(1);
         }
 
         //left 0011
         else if (speech_IO7 == 1 && speech_IO8 == 1 && speech_IO9 == 0)
         {
-            //Blue
-            redLED = 1;
-            greenLED = 1;
-            blueLED = 0;
+            if(ping1 > 15)
+            {
+                //Blue
+                redLED = 1;
+                greenLED = 1;
+                blueLED = 0;
 
-            //Turn left
-            servo_left = -5;
-            servo_right = -100;
+                //Turn left
+                servo_left = -20;
+                servo_right = -100;
 
-            uLCD.cls();
-            uLCD.printf("\nTurning left\n");
+                uLCD.cls();
+                uLCD.printf("\nTurning left\n");
 
-            xbee.printf("Turning left   \n");
+                xbee.printf("Turning left   \n");
+            }
+            else
+            {
+                uLCD.cls();
+                uLCD.printf("\nObject detected\n");
+                uLCD.printf("\nMove back\n");
+
+                xbee.printf("Object detected\n");
+            }
 
             wait(1);
         }
@@ -100,20 +122,30 @@ int main()
         //right 0100
         else if (speech_IO7 == 0 && speech_IO8 == 0 && speech_IO9 == 1)
         {
-            //Green + red
-            redLED = 0;
-            greenLED = 0;
-            blueLED = 1;
+            if(ping1 > 15)
+            {
+                //Green + red
+                redLED = 0;
+                greenLED = 0;
+                blueLED = 1;
 
-            //Turn right
-            servo_left = 100;
-            servo_right = 5;
+                //Turn right
+                servo_left = 100;
+                servo_right = 20;
 
-            uLCD.cls();
-            uLCD.printf("\nTurning right\n");
+                uLCD.cls();
+                uLCD.printf("\nTurning right\n");
 
-            xbee.printf("Turning right  \n");
+                xbee.printf("Turning right  \n");
+            }
+            else
+            {
+                uLCD.cls();
+                uLCD.printf("\nObject detected\n");
+                uLCD.printf("\nMove back\n");
 
+                xbee.printf("Object detected\n");
+            }
             wait(1);
         }
 
